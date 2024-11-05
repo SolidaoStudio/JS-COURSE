a13363494b2ea67c5608d2009f69bd097076ba2c
Luan <Luans.almeida@outlook.com>
2024-11-05 04:16:00 -0300

    Exercise 7 - New Style

--- Exercises/script/07-todo-list.js
+++ Exercises/script/07-todo-list.js
@@ -3,21 +3,31 @@ let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 function addTodo() {
   //vars
-  const inputName = document.querySelector("#todoName");
+  const name = document.querySelector("#todoName").value;
+  const date = document.querySelector("#todoDate").value;
 
-  todoList.push(inputName.value);
+  todoList.push({ name, date });
   console.log(todoList);
 
-  inputName.value = "";
+  document.querySelector("#todoName").value = "";
   localStorage.setItem("todoList", JSON.stringify(todoList));
   print();
 }
 
 function print() {
-  let printTodo = [];
-
+  let printTodo = "";
   for (let i = 0; i < todoList.length; i++) {
-    printTodo += `<p>${todoList[i]}</p>`;
+    const todoObject = todoList[i];
+    const { name, date } = todoObject;
+
+    printTodo += `
+      <span>${name}</span>
+      <span>${date}</span>
+      <button class="deleteButton" onclick="
+        todoList.splice(${i}, 1);
+        print();
+      ">Delete</button>
+    `;
   }
 
   document.getElementById("todoListDiv").innerHTML = printTodo;
