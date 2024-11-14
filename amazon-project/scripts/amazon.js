cbe120185203ea5a9d3c82b36d96a82fb6b81d8b
Luan <Luans.almeida@outlook.com>
2024-11-14 06:52:21 -0300

    Final Project - Add to Cart

--- amazon-project/scripts/amazon.js
+++ amazon-project/scripts/amazon.js
@@ -1,64 +1,30 @@
-let products = [
-  {
-    image: "./images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 45,
-      count: 87,
-    },
-    princeCents: 1090,
-  },
-  {
-    image: "./images/products/intermediate-composite-basketball.jpg",
-    name: "product-name limit-text-to-2-lines",
-    rating: {
-      stars: 40,
-      count: 127,
-    },
-    princeCents: 2095,
-  },
-  {
-    image: "./images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 45,
-      count: 54,
-    },
-    princeCents: 799,
-  },
-];
-
 function render() {
   let productList = "";
-  products.forEach(function (value) {
-    const { image } = value;
-    const { name } = value;
-    const { stars } = value.rating;
-    const { count } = value.rating;
-    const price = value.princeCents / 100;
-
+  products.forEach((product) => {
     productList += `
         <div class="product-container">
             <div class="product-image-container">
                 <img
                     class="product-image"
-                    src="${image}"
+                    src="${product.image}"
                 />
             </div>
 
-            <div class="${name}">
+            <div class="${product.name}">
             Adults Plain Cotton T-Shirt - 2 Pack
             </div>
 
             <div class="product-rating-container">
                 <img
                     class="product-rating-stars"
-                    src="images/ratings/rating-${stars}.png"
+                    src="images/ratings/rating-${product.rating.stars * 10}.png"
                 />
-                <div class="product-rating-count link-primary">${count}</div>
+                <div class="product-rating-count link-primary">${
+                  product.rating.count
+                }</div>
             </div>
 
-            <div class="product-price">${price}</div>
+            <div class="product-price">${product.priceCents / 100}</div>
 
             <div class="product-quantity-container">
                 <select>
@@ -82,17 +48,38 @@ function render() {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">Add to Cart</button>
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+              product.id
+            }">Add to Cart</button>
     </div>
   `;
-    /*
-    console.log(image);
-    console.log(name);
-    console.log(stars);
-    console.log(count);
-    console.log(price);
-    */
   });
+
   document.querySelector(".products-grid").innerHTML = productList;
 }
+
 render();
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    let matchItem;
+
+    cart.forEach((item) => {
+      if (productId === item.id) {
+        matchItem = item;
+      }
+    });
+
+    if (matchItem) {
+      matchItem.quantity += 1;
+    } else {
+      cart.push({
+        id: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+  });
+});
